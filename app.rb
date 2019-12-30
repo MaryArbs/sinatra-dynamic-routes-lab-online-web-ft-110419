6f46ae705625a5932b7de994a37d599a652371ff
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-12-30 18:55:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -27,7 +27,7 @@ class App < Sinatra::Base
     @word3 = params[:word3]
     @word4 = params[:word4]
     @word5 = params[:word5]
-    "#{@word1} #{@w2} #{@w3} #{@w4} #{@w5}."
+    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
 
   end
 
