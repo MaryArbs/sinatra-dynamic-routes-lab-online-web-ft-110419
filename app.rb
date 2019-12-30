b503ce84ee7c878aa1c37ea8469e49017eb5551c
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-12-30 19:28:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -36,9 +36,9 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @number1 = params[:number].to_i
     @number2 = params[:number].to_i
-  
+
     case @operation
-  
+
      when "add"
       (@number1 + @number2).to_s
      when "subtract"
