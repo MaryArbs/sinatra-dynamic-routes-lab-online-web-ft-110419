1bbe70beee2a8f49a0cac643133695f8e2e06fb4
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-12-30 19:10:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -31,8 +31,8 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-    @num1 = params[:number]
-    @num2 = params[:number]
+    @num1 = params[:number].t0_i
+    @num2 = params[:number].to_i
 
     add = @num1 + @num2
 
