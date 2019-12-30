1be8d1caf56d9ef7a415ca74734f22f927a603ea
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-12-30 19:24:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -40,11 +40,11 @@ class App < Sinatra::Base
      when "add"
       (@number1 + @number2).to_s
      when "subtract"
-      (@number1 - @number2)
+      (@number1 - @number2)tp_s
      when "multiply"
-      (@number1 * @number2)
+      (@number1 * @number2).to_s
      when "divide"
-      (@number1/@number2)
+      (@number1/@number2).to_s
     end
 
 
